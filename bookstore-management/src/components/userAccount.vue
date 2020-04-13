a7478aef0e28ca1229d8d001b39809b3bb9971a0
yshuf <1341289162@qq.com>
2020-04-13 08:16:30 +0800

    账号

--- bookstore-management/src/components/userAccount.vue
+++ bookstore-management/src/components/userAccount.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="page-content">
     <p class="page-title">用户账号</p>
-    <div></div>
+    <div class='search'> 
+      <div class="selectInput">
+        <el-input placeholder="请输入姓名/手机" v-model="key" class="input-with-select">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </div>
+      <div class="select">
+          <p class="title">状态 :</p>
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+      </div>
+      <div class="select addBtn"> 
+        <el-button type="primary">添加账号</el-button>
+      </div>
+    </div>
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop="id" label="序号" width="180"></el-table-column>
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
@@ -11,7 +31,7 @@
       <el-table-column prop="date" label="创建时间" width="180"></el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
           <el-button type="text" size="small">编辑</el-button>
         </template>
       </el-table-column>
@@ -52,12 +72,43 @@ export default {
         address: '上海市普陀区金沙江路 1516 弄',
         phone: '18870772596',
         status: '停用',
-      }]
+      }],
+      options: [{
+          value: '1',
+          label: '停用'
+        }, {
+          value: '2',
+          label: '启用'
+        }],
+        value: '',
+        key:''
     }
   }
 }
 </script>
 
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.search {
+  display:flex;
+  margin-bottom: 10px;
+}
+
+.selectInput,.select  {
+   display: flex;
+   align-items: center;
+}
+
+.select {
+  margin-left: 20px;
+}
+
+.addBtn {
+   text-align: right;
+ }
+
+.title {
+  font-size: 16px;
+  margin-right: 8px;
+}
 </style>
