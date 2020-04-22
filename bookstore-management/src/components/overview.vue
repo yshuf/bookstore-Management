237d5911c37906c8f80844c931cacc0a8b890f56
“ <1341289162@qq.com>
2020-04-22 22:08:28 +0800

    修改

--- bookstore-management/src/components/overview.vue
+++ bookstore-management/src/components/overview.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="overview_container">
-    <div>统计概览页面</div>
+    <div class="title">统计概览页面</div>
     <!-- <div :id="id" style="width:100%" /> -->
     <div class="dataCharts">
       <div class="chart" id="lineCharts" style="height:300px;" />
-      <div id="bookRank" style="height:300px;"> </div>
+      <div id="bookRank" style="height:300px;"></div>
     </div>
   </div>
 </template>
@@ -12,31 +12,36 @@
 <script>
 export default {
   name: 'Overview',
-  data () {
-    return { 
+  data() {
+    return {
       id: '',
-      myChart: null,
+      myChart: null
     }
   },
-   created () {
-    this.id = Math.random().toString(36).substr(2);
+  created() {
+    this.id = Math.random()
+      .toString(36)
+      .substr(2)
   },
-  mounted () {
+  mounted() {
     this.drawLine()
   },
   methods: {
-    drawLine () {
-      let myChart = this.$echarts.init(document.getElementById('lineCharts'));
-      let bookRankChart = this.$echarts.init(document.getElementById('bookRank'));
+    drawLine() {
+      let myChart = this.$echarts.init(document.getElementById('lineCharts'))
+      let bookRankChart = this.$echarts.init(
+        document.getElementById('bookRank')
+      )
       myChart.setOption({
         color: ['#3398DB'],
-        title:{
-          text:'最近一周销售情况'
+        title: {
+          text: '最近一周销售情况'
         },
         tooltip: {
           trigger: 'axis',
-          axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
           }
         },
         grid: {
@@ -67,53 +72,65 @@ export default {
             data: [10, 52, 200, 334, 390, 330, 220]
           }
         ]
-      });
+      })
       bookRankChart.setOption({
+        color: [
+          '#00b793',
+          'rgba(0, 183, 147, 0.8)',
+          'rgba(0, 183, 147, 0.6)',
+          'rgba(0, 183, 147, 0.4)',
+          'rgba(0, 183, 147, 0.2)'
+        ],
         tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b}: {c} ({d}%)'
-    },
-    legend: {
-        orient: 'vertical',
-        left: 10,
-        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-    },
-    series: [
-        {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 10,
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
             name: '访问来源',
             type: 'pie',
             radius: ['50%', '70%'],
             avoidLabelOverlap: false,
             label: {
-                show: false,
-                position: 'center'
+              show: false,
+              position: 'center'
             },
             emphasis: {
-                label: {
-                    show: true,
-                    fontSize: '30',
-                    fontWeight: 'bold'
-                }
+              label: {
+                show: true,
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
             },
             labelLine: {
-                show: false
+              show: false
             },
             data: [
-                {value: 335, name: '直接访问'},
-                {value: 310, name: '邮件营销'},
-                {value: 234, name: '联盟广告'},
-                {value: 135, name: '视频广告'},
-                {value: 1548, name: '搜索引擎'}
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 234, name: '联盟广告' },
+              { value: 135, name: '视频广告' },
+              { value: 1548, name: '搜索引擎' }
             ]
-        }
-    ]
+          }
+        ]
       })
     }
-  },
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.title {
+  padding: 20px;
+  font-size: 18px;
+  background: #fff;
+}
 .dataCharts {
   width: 100%;
   display: flex;
@@ -123,5 +140,5 @@ export default {
     padding: 20px;
     width: 45%;
   }
-}
-</style>>
+}</style
+>>
--- bookstore-management/src/components/userAccount.vue
+++ bookstore-management/src/components/userAccount.vue
@@ -3,7 +3,11 @@
     <p class="page-title">用户账号</p>
     <div class="search">
       <div class="selectInput">
-        <el-input placeholder="请输入姓名/手机" v-model="key" class="input-with-select">
+        <el-input
+          placeholder="请输入姓名/手机"
+          v-model="key"
+          class="input-with-select"
+        >
           <el-button slot="append" icon="el-icon-search"></el-button>
         </el-input>
       </div>
@@ -19,20 +23,40 @@
         </el-select>
       </div>
       <div class="select addBtn">
-        <el-button type="primary" @click="addAndEdit('add')">添加账号</el-button>
+        <el-button type="primary" @click="addAndEdit('add')"
+          >添加账号</el-button
+        >
       </div>
     </div>
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop="id" label="序号" width="180"></el-table-column>
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="phone" label="手机号" width="180"></el-table-column>
+      <el-table-column
+        prop="phone"
+        label="手机号"
+        width="180"
+      ></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
       <el-table-column prop="status" label="状态" width="180"></el-table-column>
-      <el-table-column prop="date" label="创建时间" width="180"></el-table-column>
+      <el-table-column
+        prop="date"
+        label="创建时间"
+        width="180"
+      ></el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
-          <el-button type="text" size="small" @click="addAndEdit('edit')">编辑</el-button>
+          <el-popconfirm
+            confirmButtonText="好的"
+            cancelButtonText="不用了"
+            icon="el-icon-info"
+            iconColor="red"
+            title="这是一段内容确定删除吗？"
+            onConfirm="handleClick(scope.row)"
+            ><el-button type="text" size="small">删除</el-button></el-popconfirm
+          >
+          <el-button type="text" size="small" @click="addAndEdit('edit')"
+            >编辑</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -40,19 +64,27 @@
     <!-- 表单对话框 -->
     <el-dialog :title="titleName" :visible.sync="dialogFormVisible">
       <el-form :model="form">
-        <el-form-item label="活动名称" :label-width="formLabelWidth">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
           <el-input v-model="form.name" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="活动区域" :label-width="formLabelWidth">
+        <el-form-item label="手机号" :label-width="formLabelWidth">
           <el-select v-model="form.region" placeholder="请选择活动区域">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="状态" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >确 定</el-button
+        >
       </div>
     </el-dialog>
   </div>
@@ -61,49 +93,57 @@
 <script>
 export default {
   name: 'UserAccount',
-  data () {
+  data() {
     return {
       value: '',
       key: '',
       dialogFormVisible: false,
       titleName: '',
       formLabelWidth: '120px',
-      tableData: [{
-        id: 1,
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        phone: '18870772596',
-        status: '停用',
-      }, {
-        id: 2,
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄',
-        phone: '18870772596',
-        status: '停用',
-      }, {
-        id: 3,
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄',
-        phone: '18870772596',
-        status: '停用',
-      }, {
-        id: 4,
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄',
-        phone: '18870772596',
-        status: '停用',
-      }],
-      options: [{
-        value: '1',
-        label: '停用'
-      }, {
-        value: '2',
-        label: '启用'
-      }],
+      tableData: [
+        {
+          id: 1,
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          phone: '18870772596',
+          status: '停用'
+        },
+        {
+          id: 2,
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          phone: '18870772596',
+          status: '停用'
+        },
+        {
+          id: 3,
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          phone: '18870772596',
+          status: '停用'
+        },
+        {
+          id: 4,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          phone: '18870772596',
+          status: '停用'
+        }
+      ],
+      options: [
+        {
+          value: '1',
+          label: '停用'
+        },
+        {
+          value: '2',
+          label: '启用'
+        }
+      ],
       form: {
         name: '',
         region: '',
@@ -113,20 +153,17 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      },
+      }
     }
   },
   methods: {
-    addAndEdit (type) {
-      this.dialogFormVisible = true;
+    addAndEdit(type) {
+      this.dialogFormVisible = true
       this.titleName = type === 'add' ? '添加账号' : '编辑账号'
-
     }
-  },
-
+  }
 }
 </script>
-
 
 <style lang="less" scoped>
 .search {
@@ -153,7 +190,7 @@ export default {
   margin-right: 8px;
 }
 
-.el-dialog {
+.el-dialog__wrapper .el-dialog {
   width: 420px !important;
 }
 
